3c2e3e7d6524c28aded065e12a9d5ab52bf7bf2c
Paomittsu <philip.figueroa@obf.ateneo.edu>
2023-11-18 15:02:39 +0800

    Movement and Cam changes (normalized)

--- ASUS_3rdPersonShooter/Assets/Scripts/AimStateManager.cs
+++ ASUS_3rdPersonShooter/Assets/Scripts/AimStateManager.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class AimStateManager : MonoBehaviour
 {
-    public Cinemachine.AxisState xAxis, yAxis;
+    //public Cinemachine.AxisState xAxis, yAxis;
+    float xAxis, yAxis;
     [SerializeField] Transform camFollowPos;
+    [SerializeField] float mouseSens = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +18,16 @@ public class AimStateManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xAxis.Update(Time.deltaTime);
-        yAxis.Update(Time.deltaTime);
+        //xAxis.Update(Time.deltaTime);
+        //yAxis.Update(Time.deltaTime);
+        xAxis += Input.GetAxisRaw("Mouse X") * mouseSens;
+        yAxis -= Input.GetAxisRaw("Mouse Y") * mouseSens;
+        yAxis = Mathf.Clamp(yAxis, -80, 80);
     }
 
     private void LateUpdate()
     {
-        camFollowPos.localEulerAngles = new Vector3(yAxis.Value, camFollowPos.localEulerAngles.y, camFollowPos.localEulerAngles.z);
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis.Value, transform.eulerAngles.z);
+        camFollowPos.localEulerAngles = new Vector3(yAxis, camFollowPos.localEulerAngles.y, camFollowPos.localEulerAngles.z);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis, transform.eulerAngles.z);
     }
 }

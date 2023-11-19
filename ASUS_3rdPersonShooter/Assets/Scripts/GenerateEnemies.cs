66bf44425d0b12ea42010ac8b086d98f1586a27a
Paomittsu <philip.figueroa@obf.ateneo.edu>
2023-11-20 00:35:31 +0800

    sussy tree

--- ASUS_3rdPersonShooter/Assets/Scripts/GenerateEnemies.cs
+++ ASUS_3rdPersonShooter/Assets/Scripts/GenerateEnemies.cs
@@ -8,6 +8,7 @@ public class GenerateEnemies : MonoBehaviour
 
     public GameObject theEnemy;
     public int xPos, zPos, enemyCount;
+    [SerializeField] int xParam1, xParam2, zParam1, zParam2;
 
 
     // Start is called before the first frame update
@@ -20,8 +21,8 @@ public class GenerateEnemies : MonoBehaviour
     {
         while (enemyCount < 5)
         {
-            xPos = Random.Range(40, 54);
-            zPos = Random.Range(24, 32);
+            xPos = Random.Range(xParam1, xParam2);
+            zPos = Random.Range(zParam1, zParam2);
 
             Instantiate(theEnemy, new Vector3(xPos, 0, zPos), Quaternion.identity);
             yield return new WaitForSeconds(10);

a918dfb74f02e255f4478b01333aeed6f1589054
unlimitedglassworks <raphael.stacruz@obf.ateneo.edu>
2023-11-19 22:57:32 +0800

    Buildings

--- ASUS_3rdPersonShooter/Assets/Scripts/States/WalkState.cs
+++ ASUS_3rdPersonShooter/Assets/Scripts/States/WalkState.cs
@@ -12,7 +12,7 @@ public class WalkState : MovementBaseState
     public override void UpdateState(MovementStateManager movement)
     {
         if (Input.GetKey(KeyCode.LeftShift)) ExitState(movement, movement.Run);
-        else if (Input.GetKeyDown(KeyCode.C)) ExitState(movement, movement.Crouch);
+        else if (Input.GetKeyDown(KeyCode.LeftControl)) ExitState(movement, movement.Crouch);
         else if (movement.dir.magnitude < 0.1f) ExitState(movement, movement.Idle);
 
         if(movement.vInput < 0) movement.currentMoveSpeed = movement.walkBackSpeed;

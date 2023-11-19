1fb88819d675aae86e9981bfe130e86829920780
unlimitedglassworks <raphael.stacruz@obf.ateneo.edu>
2023-11-19 17:51:12 +0800

    Ammo System

--- ASUS_3rdPersonShooter/Assets/Scripts/WeaponManager.cs
+++ ASUS_3rdPersonShooter/Assets/Scripts/WeaponManager.cs
@@ -19,24 +19,32 @@ public class WeaponManager : MonoBehaviour
 
     [SerializeField] AudioClip gunShot;
     AudioSource audioSource;
+    WeaponAmmo ammo;
+    ActionStateManager actions;
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         aim = GetComponentInParent<AimStateManager>();
+        ammo = GetComponent<WeaponAmmo>();
         fireRateTimer = fireRate;
+        actions = GetComponentInParent<ActionStateManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (ShouldFire()) Fire();
+        Debug.Log(ammo.currentAmmo);
     }
 
     bool ShouldFire()
     {
         fireRateTimer += Time.deltaTime;
         if(fireRateTimer < fireRate) return false;
+        if (ammo.currentAmmo == 0) return false;
+        if (actions.currentState == actions.Reload) return false;
         if (semiAuto && Input.GetKeyDown(KeyCode.Mouse0)) return true;
         if (!semiAuto && Input.GetKey(KeyCode.Mouse0)) return true;
         return false;
@@ -47,6 +55,7 @@ public class WeaponManager : MonoBehaviour
         fireRateTimer = 0;
         barrelPos.LookAt(aim.aimPos);
         audioSource.PlayOneShot(gunShot);
+        ammo.currentAmmo--;
         for (int i = 0; i < bulletsPerShot; i++)
         {
             GameObject currentBullet = Instantiate(bullet, barrelPos.position, barrelPos.rotation);
